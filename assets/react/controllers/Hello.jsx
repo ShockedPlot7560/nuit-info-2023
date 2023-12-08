29aa755a770a906ac3c706d76bd0b28dc928b96b
Augustin Pasquier <pasquieraugustin@outlook.com>
2023-12-08 07:25:12 +0100

    Style improvements

--- assets/react/controllers/Hello.jsx
+++ assets/react/controllers/Hello.jsx
@@ -7,14 +7,14 @@ export default function (props) {
     return (
         <div>
             <div className="home-container bg-home">
-                <h1>{i18n.t("home.welcome")}</h1>
-                <span>{i18n.t("home.subtitle")}</span>
-                <Button label={i18n.t("home.start")} text raised onClick={() => {
+                <h1 className="text-5xl font-black text-white" style={{textShadow: "3px 3px 7px rgba(0, 0, 0, 0.8)"}}>{i18n.t("home.welcome")}</h1>
+                <span className="text-xl text-white" style={{textShadow: "3px 3px 7px rgba(0, 0, 0, 0.8)", maxWidth: "55vw", textAlign: "center"}}>{i18n.t("home.subtitle")}</span>
+                <Button className="mt-5" size="large" label={i18n.t("home.start")} text raised onClick={() => {
                     document.location.href = "/intro";
                 }} />
             </div>
-            <img style={{position: "absolute", bottom: 0, left: 0}} src={"/img/homepage/plant.svg"} />
-            <img style={{position: "absolute", top: 0, right: 0, transform: "rotate(180deg)"}} src={"/img/homepage/plant.svg"} />
+            <img style={{position: "absolute", bottom: 0, left: 0, maxWidth: "100vw", maxHeight: "100vh"}} src={"/img/homepage/plant.svg"} />
+            <img style={{position: "absolute", top: 0, right: 0, transform: "rotate(180deg)", maxWidth: "100vw", maxHeight: "100vh"}} src={"/img/homepage/plant.svg"} />
         </div>
     )
 }
