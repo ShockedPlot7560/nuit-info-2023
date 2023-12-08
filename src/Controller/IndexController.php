438f1bdf99224d74dd0e94a8cffc5b67bf7824d8
ShockedPlot7560 <no-reply@tchallon.fr>
2023-12-08 04:54:59 +0100

    include index + introduction

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,7 +17,7 @@ class IndexController extends AbstractController
     {
     }
 
-    #[Route('/index', name: 'app_index')]
+    #[Route('/', name: 'app_index')]
     public function index(Request $request): Response
     {
         return $this->render('index/index.html.twig', [
